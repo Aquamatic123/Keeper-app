beca079f32333dadb0f2a28dd8b9205eea4c408f
Leo Rouleau <leorouleau5070@gmail.com>
2025-10-08 16:30:01 -0400

    add creation area and deleting area for notes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-
 function Footer() {
-    const currentYear = new Date().getFullYear();
-    return <p>
-        copyright © {currentYear}
-    </p>
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ⓒ {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
